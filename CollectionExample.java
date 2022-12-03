2bc44e8a1ca958a0d5cb10032bf19ddd2b678daa
arpita <arpitapsamal1999@gmail.com>
2022-12-03 12:06:22 +0530

    added CollectionExample.java

---
+++ CollectionExample.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionExample {
+    public static void main(String args[])
+    {
+        Map<String,Integer> mapList=new HashMap<>();
+        mapList.put("Arpita",1);
+        mapList.put("Abhipsa",2);
+        mapList.put("Priyanshi",3);
+        System.out.println(mapList);
+    }
+}
