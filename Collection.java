7b523caf3fe695d4eaffbfadaeba1c922b6ba12d
arpita <arpitapsamal1999@gmail.com>
2022-12-03 11:38:57 +0530

    added collection.java

---
+++ Collection.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection {
+    public static void main(String[] args) {
+
+
+        List<String> list = new ArrayList<>();
+        list.add("Ankita");
+        list.add("Arpita");
+        list.add("PRIYANKA");
+        System.out.println(list);
+    }
+}
